8149ecc99700203f5dc17b65e167b8019cb1769a
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-03-18 22:35:10 -0400

    Update scheduler.h

--- include/scheduler.h
+++ include/scheduler.h
@@ -7,5 +7,6 @@
 // Declare the task scheduling functions
 void add_task(void (*task_func)(void));  // Add a task to the scheduler
 void run_scheduler(void);                // Run the round-robin scheduler
+void init_scheduler(void);
 
 #endif  // SCHEDULER_H
